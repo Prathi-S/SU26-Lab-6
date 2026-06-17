70fd3acc52d7e017bdd8d83cf23d6b35e62a2f3a
qoride <qoride@gmail.com>
2026-06-17 00:38:10 +0000

    seed data finished, starting on app routes

--- Lab6/app.py
+++ Lab6/app.py
@@ -38,7 +38,7 @@ def course_to_dict(course):
     return{
         "id": course.id,
         "course_name": course.course_name,
-        "teaacher": course.teacher.name,
+        "teacher": course.teacher.name,
         "time": course.time,
         "students_enrolled": enrolled,
         "capacity": course.capacity
@@ -48,4 +48,42 @@ def seed_data():
     if User.query.first():
         return
 
+    anthony = User(username="aliu", password="password", name="Anthony Liu", role="student")
+    vishnu = User(username="vdeva", password="password", name="Vishnu Devarapalli", role="student")
+    prathi = User(username="psatti", password="password", name="Prathi Sattiamoorthy", role="student")
+
+    hepworth = User(username="ahepworth", password="password", name="Dr Hepworth", role="teacher")
+
+    admin = User(username="admin", password="password", name="Jane Smith", role="admin")
+
+    db.session.add_all({anthony,vishnu,prathi,
+                        hepworth,
+                        admin})
+    db.session.commit()
+
+    cse108 = Course(course_name="Full Stack Web Development", teacher_id=hepworth.id, time="MWF 10:00 AM - 12:20 PM", capacity=64)
+
+    db.session.add_all({cse108})
+    db.session.commit()
+
+    enrollments = [
+        Enrollment(student_id=anthony.id, course_id=cse108.id, grade=100)
+    ]
+
+    db.session.add_all(enrollments)
+    db.session.commit()
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/login", methods=["POST"])
+def login():
+    data = request.get_json()
+    user = User.query.filter_by(username=data["username"], password=data["password"]).first()
+
+    if user:
+        session["user_id"] = user.id
+        return json.dumps({"success": True, "name": user.name, "role": user.role})
     
+    return json.dumps({"success": False, "error": "Invalid username or password"})
